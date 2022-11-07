caa55426028b6194774649d316d4379ed5f30804
df2df <106992996+df2df@users.noreply.github.com>
2022-11-07 01:57:10 -0500

    Update ddim.py

--- ldm/models/diffusion/ddim.py
+++ ldm/models/diffusion/ddim.py
@@ -139,7 +139,7 @@ class DDIMSampler(object):
 
         for i, step in enumerate(iterator):
             index = total_steps - i - 1
-            ts = torch.full((b,), step, device=device, dtype=torch.long)
+            ts = torch.full((b,), step, device=device, dtype=torch.float16)
 
             if mask is not None:
                 assert x0 is not None
